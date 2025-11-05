ccc9aa1224cf7ade61f1553d3db400c077bc68a0
kanishikaverma506 <kanishikaverma8@gmail.com>
2025-11-05 23:38:32 +0530

    client side

--- client.c
+++ client.c
@@ -12,17 +12,17 @@ void main(){
     int clientSocket ;
     struct sockaddr_in serverAddr; //structure to hold server address details
     char buffer[1024]; //data buffer to hold data
-    clientSocket= socket(PF_INIT,SOCK_STREAM,0); //create socket
+    clientSocket= socket(PF_INET,SOCK_STREAM,0); //create socket
     /* pf_init = protocol family , sock_stream is for tcp ,sock_datagram for udp , 0 for any best protocol*/
-    print("client socket created successfully\n");
-    memset(serverAddr ,"\0" ,sizeof(serverAddr)); //initialize server address structure with null value
+    printf("client socket created successfully\n");
+    memset(&serverAddr ,"\0" ,sizeof(serverAddr)); //initialize server address structure with null value
     serverAddr.sin_family=AF_INET;
     serverAddr.sin_port=htons(PORT); //host to network short conversion for port number
-    serverAddr.sin_addr.s_addr=inet_addr(127.0.0.1); //convert ip address to binary format and store in server address structure
+    serverAddr.sin_addr.s_addr=inet_addr("127.0.0.1"); //convert ip address to binary format and store in server address structure
     //connect to server
     connect(clientSocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
     printf("Connected to server successfully\n");
     recv(clientSocket,buffer,1024,0);
     printf("data received  %s \n",buffer);
     printf("Client exiting....\n");
-}  
+    }  
