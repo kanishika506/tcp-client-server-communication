ada4c7eaf521db89328511e22e35b0c5dface38e
kanishikaverma506 <kanishikaverma8@gmail.com>
2025-11-14 22:59:29 +0530

    udp client side

--- UDP-client-server/udp_client.py
+++ UDP-client-server/udp_client.py
@@ -3,11 +3,9 @@ import socket
 # Create UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('127.0.0.1', 9999)
-
-while True:
-    msg = input("Enter message for server: ")
-    client_socket.sendto(msg.encode(), server_address)  # Send msg to server
-
-    data, server = client_socket.recvfrom(1024)         # Receive reply
-    print("Server:", data.decode())
+msg="HELLO ! UDP SERVER"
+client_socket.sendto(msg.encode('utf-8'),('127.0.0.1',12345))
+data,addr=client_socket.recvfrom(1024)
+print("SERVER SAYS:")
+print(str(data))
+client_socket.close()
